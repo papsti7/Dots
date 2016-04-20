e54370aab815739cd2572fb9d24fa3335fbbfec2
Stefan Papst <stefan.papst@student.tugraz.at>
2016-04-20 15:34:55 +0200

    deleted old project and added new libgdx project

---
+++ android/src/com/swwm/defaultteam/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.swwm.defaultteam;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.swwm.defaultteam.startPoint;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new startPoint(), config);
+	}
+}
