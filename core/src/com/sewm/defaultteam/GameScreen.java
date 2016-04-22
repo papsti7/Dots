d1047496f37ec4ed13b696eb736336a7963ed15e
Stefan Papst <stefan.papst@gmx.net>
2016-04-22 13:18:44 +0200

    [MT,SP]created GameScreen

---
+++ core/src/com/sewm/defaultteam/GameScreen.java
@@ -0,0 +1,51 @@
+package com.sewm.defaultteam;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+
+/**
+ * Created by stefan on 22.04.2016.
+ */
+public class GameScreen implements Screen {
+    World world_;
+    WorldRenderer worldRenderer_;
+    @Override
+    public void show() {
+        world_ = new World();
+        worldRenderer_ = new WorldRenderer(world_);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        //System.out.println("render\n");
+        worldRenderer_.render();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
