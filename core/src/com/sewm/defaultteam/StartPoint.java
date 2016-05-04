d58a20af485af454fb81ba23bc2316b54c5d02cf
Stefan Papst <stefan.papst@student.tugraz.at>
2016-05-04 12:10:31 +0200

    [SP,MT] added GameOverScreen Class

--- core/src/com/sewm/defaultteam/StartPoint.java
+++ core/src/com/sewm/defaultteam/StartPoint.java
@@ -6,8 +6,12 @@ import com.badlogic.gdx.Game;
 
 public class StartPoint extends Game{
 
+    public static StartPoint startPoint_;
     @Override
     public void create() {
+
+        startPoint_ = this;
         setScreen(new GameScreen());
     }
+
 }
